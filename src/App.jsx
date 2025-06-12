fa4d007fb60cb31f89a7b10df9ed55fad7d54ff8
Keith <khengquanlim@gmail.com>
2025-06-12 12:29:36 +0800

    Unused set state changed to _ instead

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import './App.css'
 import WeatherPage from './pages/Weather/WeatherPage'
 
 function App() {
-  const [theme, setTheme] = useState(() => localStorage.getItem('theme') || 'light') 
+  const [theme, _] = useState(() => localStorage.getItem('theme') || 'light') 
 
   return (
     <>
       <div className={`flex items-center justify-center min-h-screen w-full bg-cover bg-center ${
-        theme === 'light'
-          ? "bg-[url('/bg-light.png')]"
-          : "bg-[url('/bg-dark.png')]"}`}>
-        <WeatherPage />
+      theme === 'light'
+        ? "bg-[url('/bg-light.png')]"
+        : "bg-[url('/bg-dark.png')]"}`}>
+      <WeatherPage />
       </div>
     </>
   )
